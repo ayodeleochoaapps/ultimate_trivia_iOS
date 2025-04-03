5ec0364e99ef5e9c4d2e4b8a6daebf1ac238361c
ayodeleochoa <ayo@ayodeleochoa.com>
2025-04-02 21:43:11 -0400

    Initial Commit

---
+++ UltimateTriviaTests/UltimateTriviaTests.swift
@@ -0,0 +1,17 @@
+//
+//  UltimateTriviaTests.swift
+//  UltimateTriviaTests
+//
+//  Created by Ayodele Ochoa on 4/2/25.
+//
+
+import Testing
+@testable import UltimateTrivia
+
+struct UltimateTriviaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
